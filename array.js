60e6af90069f3c20fb3796f5e3ca86f840204718
Rowen Scout <rowenscout@gmail.com>
2017-06-26 15:19:13 -0700

    added some really annoying alerts. they're just placeholders for now. Eventlisteners are up and running through.

--- array.js
+++ array.js
@@ -1,19 +1,33 @@
 'use strict';
 
+//declaring global variables
 var index = 0;
-var allItems = [];
+var items = [];
+var i = 0;
+
+
 var imageIndex = [];
+
+//this array contains our currently displayed items
 var displayItems = [];
 
+//current number of selections the user has made
+var round = 0;
+
+// a constructor function a lot like the one from Salmon Cookies, but this one
+//keeps track of it's index in the items array.
 function Item(name, path) {
   this.name = name;
   this.path = path;
-  this.clicks = 0;
   this.shown = 0;
+  this.clicks = 0;
   this.index = index++;
-  allItems.push(this);
+  items.push(this);
 }
 
+//creating all of our items using the object constructor. I really wanted
+//to declare a prototype before here but wasn't sure how to incorporate that
+//into the code without things getting bogged down.
 new Item('bag', 'assets/bag.jpg');
 new Item('banana', 'assets/banana.jpg');
 new Item('bathroom', 'assets/bathroom.jpg');
@@ -35,15 +49,19 @@ new Item('usb', 'assets/usb.gif');
 new Item('watercan', 'assets/water-can.jpg');
 new Item('wineglass', 'assets/wine-glass.jpg');
 
-var img1 = document.getElementById('one');
-var img2 = document.getElementById('two');
-var img3 = document.getElementById('three');
+// grabbing the ID of our image elements and assigning them to declared variables
+//I used the 0, 1, 2 naming scheme so that the ID can refer to the item's position
+//in an array. See line 114
+var image1 = document.getElementById('0');
+var image2 = document.getElementById('1');
+var image3 = document.getElementById('2');
 
-//generates a random image from our allItems array
+//generates a random index for items array and pushes that index into the
+//displayItems array.
 function random() {
-  var index = Math.floor(Math.random() * allItems.length);
+  var index = Math.floor(Math.random() * items.length);
   if (displayItems.indexOf(index) > 0) {
-    index = Math.floor(Math.random() * allItems.length);
+    index = Math.floor(Math.random() * items.length);
   } else {
     displayItems.push(index);
   }
@@ -51,30 +69,59 @@ function random() {
   return index;
 }
 
-function rand3() {
+//this functions sets the source of our images by refering to their paths via indices.
+//it also applies a class to them so that they resize.
+//for the 2nd and 3rd images a comparison generates new numbers if there is any
+//duplication
+function genImages() {
   imageIndex[0] = random();
-  img1.setAttribute('src', allItems[imageIndex[0]].path);
-  img1.setAttribute('class', 'choice');
+  image1.setAttribute('src', items[imageIndex[0]].path);
+  image1.setAttribute('class', 'choice');
 
   imageIndex[1] = random();
   while (imageIndex[1] === imageIndex[0])  {
     imageIndex[1] = random();
   }
 
-  img2.setAttribute('src', allItems[imageIndex[1]].path);
-  img2.setAttribute('class', 'choice');
+  image2.setAttribute('src', items[imageIndex[1]].path);
+  image2.setAttribute('class', 'choice');
 
   imageIndex[2] = random();
   while (imageIndex[2] === imageIndex[0] || imageIndex[2] === imageIndex[1]) {
     imageIndex[2] = random();
   }
 
-  img3.setAttribute('src', allItems[imageIndex[2]].path);
-  img3.setAttribute('class', 'choice');
+  image3.setAttribute('src', items[imageIndex[2]].path);
+  image3.setAttribute('class', 'choice');
 
   console.log(imageIndex[0]);
   console.log(imageIndex[1]);
   console.log(imageIndex[2]);
 }
 
-rand3();
+//calling out rand3 function which gives our image tags a source and a class.
+genImages();
+
+//adding an event listener for each displayed image which calls handleSelection on click
+image1.addEventListener('click', handleSelection);
+image2.addEventListener('click', handleSelection);
+image3.addEventListener('click', handleSelection);
+
+//declaring handleSlection. It tracks how many selections the user has made.
+//it also tracks how many times each image has been selected
+//as well as the number of times each image has been displayed.
+function handleSelection() {
+  items[imageIndex[+event.target.id]].clicks++;
+  for (i = 0; i < 2; i++) {
+    items[imageIndex[i]].numDisplays++;
+  }
+
+  round++;
+  if (round === 25) {
+    alert('Survey completed!');
+  }
+
+  var left = 25 - round;
+  alert(left + ' Selection(s) remaining.');
+  genImages();
+}
